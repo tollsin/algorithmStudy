edd74b53ae67d9f588371410853c95ee9abad75e
heekyung <tollsin@Nate.com>
2019-04-15 15:05:34 +0900

    Create Treasure.java
    baejoon 1026

---
+++ src/backjoon/Treasure.java
@@ -0,0 +1,27 @@
+package backjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Treasure {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		int b[] = new int[n];
+		for(int i=0;i <n ;i++) {
+			a[i] = sc.nextInt();
+		}
+		for(int i=0;i <n ;i++) {
+			b[i] = sc.nextInt();
+		}
+		Arrays.sort(a);
+		Arrays.sort(b);
+		int sum = 0;
+		for(int i=0; i<a.length; i++) {
+			sum += a[i] * b[n-1-i];
+		}
+		System.out.println(sum);
+	}	
+}
